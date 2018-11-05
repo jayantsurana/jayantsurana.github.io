cc14cec5126f1eee364d8939b25bac917ef9b949
jayantsurana <jayantplb@gmail.com>
2018-11-06 00:53:29 +0530

    added a react app distribution/build

---
+++ precache-manifest.735f529027487867bf91b0758e96c8b1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c7a11c40f6d0b4a50426",
+    "url": "/static/js/main.c7a11c40.chunk.js"
+  },
+  {
+    "revision": "e41956e913ecb0fd8724",
+    "url": "/static/js/1.e41956e9.chunk.js"
+  },
+  {
+    "revision": "f5a6a03c5239168e2473ffe27af63433",
+    "url": "/index.html"
+  }
+];
